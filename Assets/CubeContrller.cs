5b97de01d7d2bb625b9034258af0501f4a032e9b
Eiji Yamashita <taku09035609949@icloud.com>
2018-12-21 16:02:38 +0900

    GoForwardUnityChan project firstコミット
    ２Dゲームで障害物を避けるゲームです

---
+++ Assets/CubeContrller.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeContrller : MonoBehaviour {
+
+	//キューブの移動速度
+	private float speed= -0.2f;
+
+	//消滅位置
+	private float deadLine= -10;
+
+	private AudioSource sound;
+
+
+
+	// Use this for initialization
+	void Start () {
+		sound = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//キューブを移動させる
+		transform.Translate(this.speed,0,0);
+
+		//画面に出たら破棄する
+		if(transform.position.x<this.deadLine){
+			Destroy (gameObject);
+		}
+		
+	}
+
+	void OnCollision2DEnter(GameObject other){
+		Debug.Log("a");
+		if (other.gameObject.tag != "Unitychan") {
+			sound.Play ();
+		} 
+
+	}
+}
---
+++ Assets/CubeGenerator.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeGenerator : MonoBehaviour {
+
+	//キューブのPrefab
+	public GameObject cubePrefab;
+
+	//時間計測用の変数
+	private float delta=0;
+
+	//時間計測用の変数
+	private float span=1.0f;
+	//キューブの生成位置
+	private float genPosX=12;
+	//キューブの生成位置オフセット
+	private float offsetY=0.3f;
+	//キューブのたて方向の感覚
+	private float spaceY=6.9f;
+	//キューブの生成位置のオフセット
+	private float offsetX=0.5f;
+	//キューブの横方向の感覚
+	private float spaceX=0.4f;
+	//キューブの生成個数の上限
+	private int maxBlockNum=4;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.delta += Time.deltaTime;
+		//span秒以上の時間が経過したかを調べる
+		if(this.delta>this.span){
+			this.delta = 0;
+			//生成するキューブ数をランダムに決める
+			int n=Random.Range(1,maxBlockNum+1);
+
+			//指定したキューブを生成する
+			for(int i=0;i<n;i++){
+				//キューブを生成する
+				GameObject go =Instantiate (cubePrefab) as GameObject;
+				go.transform.position = new Vector2 (this.genPosX, this.offsetY + i * this.spaceY);
+			}
+			//次のキューブまでの生成時間を決める
+			this.span=this.offsetX+this.spaceX*n;
+		}
+		
+	}
+}
